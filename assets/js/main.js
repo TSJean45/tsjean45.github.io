9940d657d7d31679c4daaaaa7aa5f0abf66219de
Tan Szu Jean <szujean45@gmail.com>
2022-04-02 11:43:06 +0800

    delete message

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,13 @@
 const txts = document.querySelector(".animated-text").children,
   txtsLen = txts.length;
 let i = 0;
-const slideInTimer = 3500,
-  slideOutTimer = 3500;
+const slideInTimer = 3500;
 
 function animateText() {
   for (let j = 0; j < txtsLen; j++) {
-    txts[j].classList.remove("slide-in", "slide-out");
+    txts[j].classList.remove("slide-in");
   }
   txts[i].classList.add("slide-in");
-
-  setTimeout(function () {
-    txts[i].classList.add("slide-out");
-  }, slideOutTimer)
 
   setTimeout(function () {
 
